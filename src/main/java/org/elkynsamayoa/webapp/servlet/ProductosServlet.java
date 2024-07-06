17c8b414d15b1291ee7d77f64e7e4af0c4c88595
elkyn2006 <esamayoa-2023566@kinal.edu.gt>
2024-07-05 19:31:30 -0600

    tarea

---
+++ src/main/java/org/elkynsamayoa/webapp/servlet/ProductosServlet.java
@@ -0,0 +1,9 @@
+package org.elkynsamayoa.webapp.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/producto-servlet")
+public class ProductosServlet extends HttpServlet {
+    
+}
